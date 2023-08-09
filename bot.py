09874048b0203e2de09ebfde2c2112f68b618231
KrolixW <krolikwormix@gmail.com>
2023-08-09 17:05:55 +0300

    Bot basics added

--- bot.py
+++ bot.py
@@ -1,3 +1,16 @@
+import os
+import logging
+import asyncio
+from aiogram import Dispatcher, Bot
+async def main():
+    API_TOKEN = str(os.getenv('KROLIX_BOT_TOKEN'))
+    bot = Bot(token=API_TOKEN)
+    dp = Dispatcher()
 
-main():
+    # Setup logging
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
 
